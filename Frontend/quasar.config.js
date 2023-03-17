b6cd862badc00fc21d31c18839260ce13affb883
Quinten Mortier <quinten.mortier@student.kuleuven.be>
2023-03-17 21:30:28 +0100

    frontend docker

--- Frontend/quasar.config.js
+++ Frontend/quasar.config.js
@@ -85,7 +85,7 @@ module.exports = configure(function (ctx) {
       server: {
         type: 'http'
       },
-      port: 8080,
+      port: 3786,
       open: true // opens browser window automatically
     },
 
