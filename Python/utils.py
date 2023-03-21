c1682fa4a249236ff341875355f82fb5873d8b84
Quinten Mortier <quinten.mortier@student.kuleuven.be>
2023-03-21 01:14:32 +0100

    deploy  bugs fixed

--- Python/utils.py
+++ Python/utils.py
@@ -2,15 +2,15 @@ import os
 import pickle
 
 #function that saves all image names of a folder in a list and saves it to a pickle file
-def get_image_names():
-    names = []
-    for i, image in enumerate(os.listdir("/home/mortirreke/Desktop/assets/")):
-        if image.endswith(".png") or image.endswith(".jpg") or image.endswith(".jpeg"):
-            names.append(image)
-    with open('imagelist.pickle', 'wb') as handle:
-        pickle.dump(names, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-get_image_names()
+# def get_image_names():
+#     names = []
+#     for i, image in enumerate(os.listdir("/home/mortirreke/Desktop/assets/")):
+#         if image.endswith(".png") or image.endswith(".jpg") or image.endswith(".jpeg"):
+#             names.append(image)
+#     with open('imagelist.pickle', 'wb') as handle:
+#         pickle.dump(names, handle, protocol=pickle.HIGHEST_PROTOCOL)
+#
+# get_image_names()
 
 
 #function that gets artist and title from image name (format: artist_title.jpg) or unknown if no artist or title is found
