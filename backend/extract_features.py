c97f0735eab1158e62b44a7d22f52d513f51c147
Ivania Donoso-Guzmán <ivania.donoso@gmail.com>
2024-05-10 12:03:59 +0200

    Added script to just extract features

--- backend/extract_features.py
+++ backend/extract_features.py
@@ -1,8 +1,10 @@
 import json
 
 from utils.extract_features import extract_features
+from utils import get_image_names
 
 if __name__ == '__main__':
+    get_image_names("data/high_renaissance")
     index = extract_features("data/high_renaissance")
     with open('temp/high_renaissance.json', 'w') as f:
         json.dump(index, f)
